22646d27c1b95a0c94b1dfce193b17f92ad6d03b
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-09 12:40:44 -0400

    for loop completed

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -37,10 +37,15 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
+        int counter = 0;
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for(int i = 0; i< personArray.length;i++){
+            Person currentPerson = personArray[counter];
+            result+= currentPerson.toString();
+            counter++;
+        }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -55,9 +60,14 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
+        int counter = 0;
         // identify array's type
         // identify array's variable-name
-
+        for(Person i:personArray){
+            Person currentPerson = personArray[counter];
+            result+= currentPerson.toString();
+            counter++;
+        }
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
