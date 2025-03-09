c436d935b21f999f116ea33b3c86e2a5be8e2ee1
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-09 12:21:21 -0400

    while loop completed

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -16,13 +16,19 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // create a `counter`
+        int counter = 0;
         // while `counter` is less than length of array
+        while(counter< personArray.length) {
             // begin loop
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
 
+            // use `counter` to identify the `current Person` in the array
+            Person person = personArray[counter];
+            // get `string Representation` of `currentPerson`
+            result+=person.toString();
+            // append `stringRepresentation` to `result` variable
+            counter ++;
+        }
             // end loop
         return result;
     }
